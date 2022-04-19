a78f45c2b4f6edf4edbf2dd3be741957ed50d053
Adam <adamghebrezgi@gmail.com>
2022-04-19 13:19:03 +0300

    Header file

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_percent - function that prints a percent
+ * @%: argument from _printf
+ *
+ * Return: %
+ */
+int print_percent(va_list %)
+{
+	(void)%;
+	return (_putchar('%'));
+}
