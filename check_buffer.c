b76226cb6cc9ad7a4b9764c6012cc661845bb67d
Adam <adamghebrezgi@gmail.com>
2022-04-20 10:22:15 +0300

    buffer

--- check_buffer.c
+++ check_buffer.c
@@ -18,7 +18,7 @@ char *create_buffer(void)
  * write_buffer - prints buffer, then frees it and frees va_list
  * @buffer: buffer holding print-ables
  * @len: length of print-able string
- * @list: va_list
+ * @arg: va_list
  */
 void write_buffer(char *buffer, int len, va_list arg)
 {
