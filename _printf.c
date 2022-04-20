a122f9d574edbcfcec9abb2d6a31d071bd308587
Adam <adamghebrezgi@gmail.com>
2022-04-20 10:39:43 +0300

    printf function

--- _printf.c
+++ _printf.c
@@ -7,37 +7,37 @@
  *
  * Return: pointer to valid function or NULL
  */
-static int (*get_func(const char *format))(va_list)
+int (*get_func(const char *format))(va_list)
 {
-	unsigned int i;
-	print_t type[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{"%", print_percent},
-		{NULL, NULL}
-	};
+        unsigned int i;
+        print_t type[] = {
+                {"c", print_c},
+                {"s", print_s},
+                {"i", print_i},
+                {"d", print_d},
+                {"u", print_u},
+                {"b", print_b},
+                {"o", print_o},
+                {"x", print_x},
+                {"X", print_X},
+                {"p", print_p},
+                {"S", print_S},
+                {"r", print_r},
+                {"R", print_R},
+                {"%", print_percent},
+                {NULL, NULL}
+        };
 
-	i = 0;
-	while (type[i].t[0])
-	{
-		if (type[i].t[0] == *format)
-		{
-			break;
-		}
-		i++;
-	}
-	return (type[i].f);
+        i = 0;
+        while (type[i].t[0])
+        {
+                if (type[i].t[0] == *format)
+                {
+                        break;
+                }
+                i++;
+        }
+        return (type[i].f);
 }
 
 /**
